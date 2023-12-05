712d7f83510f96ce3f1f02a66c5da51cd2da1be7
unknown <SaptaAjiSadewo@email.com>
2023-12-05 08:28:10 +0700

    commit1

---
+++ kelompok4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct Film {
+    string judul;
+    string pemeranUtama;
+    double rating;
+    int ratingUsia;
+    string genre;
+    int tahunRilis;
+    string sutradara;
+};
